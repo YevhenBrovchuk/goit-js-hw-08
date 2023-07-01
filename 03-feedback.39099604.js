7fa5b874e33b559c2c6ab5c341292b0bbcd6bd07
YevhenBrovchuk <124561578+YevhenBrovchuk@users.noreply.github.com>
2023-07-01 21:02:26 +0000

    Deploying to gh-pages from @ YevhenBrovchuk/goit-js-hw-08@a4f3cfe95db2415d81f359dc9aae61fcca38e0f4 🚀

---
+++ 03-feedback.39099604.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".feedback-form").addEventListener("input",(function(t){e[t.target.name]=t.target.value}));var e={}}();
+//# sourceMappingURL=03-feedback.39099604.js.map
---
+++ 03-feedback.8ebecc74.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(t){e[t.target.name]=t.target.value}));const e={};
+//# sourceMappingURL=03-feedback.8ebecc74.js.map
